df5a486a053466c731c987aa3787516808ab1eb5
LuzMieres <mieresluz5@gmail.com>
2024-07-08 14:21:03 -0300

    Ajusté estilos

--- moviestack.js
+++ moviestack.js
@@ -1,24 +1,24 @@
-let contenedor= document.getElementById('contenedor')
-console.log(contenedor)
+let contenedor= document.getElementById('contenedor');
+console.log(contenedor);
 
 function estructuraCard(image, title, tagline, overview) {
     return `
-    <div class="contenedor w-80 h-96 shadow-2xl border-solid border-2 border-white rounded-2xl text-center bg-violet-200 text-black">
-    <img class="h-40 w-80 object-cover rounded-t-2xl" src="${image}" alt="imagen-fruta">
+    <div class="flex flex-col items-center justify-beetwen w-72 h-auto shadow-2xl border-solid border-2 border-white rounded-2xl text-center bg-violet-200 text-black">
+    <img class="h-40 w-72 object-cover rounded-t-2xl" src="${image}" alt="imagen-fruta">
     <h4 class="font-bold">${title}</h4>
-    <p class="p-3 text-xs font-bolder">${tagline}</p>
-    <p class="pb-3 text-xs font-bolder">${overview}</p>
+    <p class="p-3 font-bolder">${tagline}</p>
+    <p class="p-3 font-bolder">${overview}</p>
     </div>`
 }
-console.log (estructuraCard)
+console.log (estructuraCard);
 
 function imprimirCard(listaMovies) {
-   let cards = ""
+   let cards = "";
    for (const movie of listaMovies) {
-    cards += estructuraCard(movie.image, movie.title, movie.tagline, movie.overview)
+    cards += estructuraCard(movie.image, movie.title, movie.tagline, movie.overview);
    }
    return cards
 }
-console.log(imprimirCard(movies))
+console.log(imprimirCard(movies));
 
-contenedor.innerHTML=imprimirCard(movies)
+contenedor.innerHTML = imprimirCard(movies);
